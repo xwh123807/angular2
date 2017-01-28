1c3d53d2836bc56155e1de47c2e89ebf1ba364e9
xiangwanhong <xiangwanhong@192.168.2.49>
2017-01-28 21:01:25 +0800

    step 1

--- samples/heroes/src/app/app.component.ts
+++ samples/heroes/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
