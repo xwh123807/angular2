cc4d36a31a8a78117ad02d5b2682a74fac52bb49
xwh123807 <xwh123807@gmail.com>
2017-06-02 00:10:53 +0800

    add routing module

--- study/angular-router/src/app/app.module.ts
+++ study/angular-router/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    HeroesModule
+    HeroesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
